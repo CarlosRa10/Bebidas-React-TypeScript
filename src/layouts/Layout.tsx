754bfc76eb630af121a62844f800ee9c28659cff
carlosra10 <carloseduardoramirez10@gmail.com>
2025-05-17 22:48:39 -0400

    Agregar comentario explicativo en el componente Layout para mejorar la claridad del código

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,6 +1,7 @@
 import {Outlet} from 'react-router-dom'// Outlet es para renderizar el contenido de las rutas hijas
 import Header from '../components/Header'
 export default function Layout() {
+  //pagina principal, layout, contenedor de las rutas hijas
   return (
     <>
          <Header/>
