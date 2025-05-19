58385792f47a3b5a4bd21d521b0c24424fc2e4b1
carlosra10 <carloseduardoramirez10@gmail.com>
2025-05-18 21:48:26 -0400

    Agregar comentarios explicativos en main.tsx para mejorar la claridad del código

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import AppRouter from './router'
-
+// aqui es donde se renderiza la aplicacion
+// el elemento root es el contenedor principal de la aplicacion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppRouter />
