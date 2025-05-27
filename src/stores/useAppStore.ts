10dd23be4d566cafe4530729a239bc03f75b641f
Carlosra10 <carloseduardoramirez10@gmail.com>
2025-05-27 09:28:28 -0400

    Actualizar comentario en useAppStore.ts para incluir 'api' en la descripción de los argumentos

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -3,7 +3,7 @@ import { createRecipeSlice } from './recipeSlice'
 // Este archivo define el store de Zustand para la aplicación
 //(...a) Es una sintaxis de JavaScript/TypeScript que captura todos los argumentos pasados a la función en un array llamado a
 //paso esta función create((...a) a createRecipeSlice(...a) para que use el store de recetas
-// ...a toma una copia de todos los argumentos , get, set etc. y los pasa a createRecipeSlice
+// ...a toma una copia de todos los argumentos , get, set y api etc. y los pasa a createRecipeSlice
 export const useAppStore = create((...a) => ({
     ...createRecipeSlice(...a)
 }))
