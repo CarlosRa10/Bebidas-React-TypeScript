eb343242fc390f8fc9f8c18572d2063543fae0ca
Carlosra10 <carloseduardoramirez10@gmail.com>
2025-05-27 10:06:45 -0400

    Agregar tipo RecipeSliceType en recipeSlice.ts y actualizar useAppStore.ts para su uso

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,5 +1,14 @@
+import type { StateCreator } from "zustand"
 
+type Category = {}
 
-export const createRecipeSlice = () => ({
+export type RecipeSliceType = {
+    categories: Category[]
+}
+
+//StateCreator<RecipeSliceType>
+//Le dice a TypeScript: "Esta función debe cumplir con la forma de un creador de estado de Zustand"
+//Y que el estado que devuelva debe coincidir con RecipeSliceType
+export const createRecipeSlice : StateCreator<RecipeSliceType> = () => ({
     categories:[]
 })
