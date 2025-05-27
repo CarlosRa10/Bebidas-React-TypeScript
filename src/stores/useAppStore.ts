80ae3214e1cc8bbd85481f06f20710285fcdf310
Carlosra10 <carloseduardoramirez10@gmail.com>
2025-05-27 09:25:59 -0400

    Agregar comentarios explicativos en useAppStore.ts para mejorar la claridad del código

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,6 +1,9 @@
 import {create} from 'zustand'
 import { createRecipeSlice } from './recipeSlice'
 // Este archivo define el store de Zustand para la aplicación
+//(...a) Es una sintaxis de JavaScript/TypeScript que captura todos los argumentos pasados a la función en un array llamado a
+//paso esta función create((...a) a createRecipeSlice(...a) para que use el store de recetas
+// ...a toma una copia de todos los argumentos , get, set etc. y los pasa a createRecipeSlice
 export const useAppStore = create((...a) => ({
     ...createRecipeSlice(...a)
 }))
