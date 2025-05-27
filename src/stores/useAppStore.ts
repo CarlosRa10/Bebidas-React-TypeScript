4ab848c7af4f448eff1c2c9122d890b547269e20
Carlosra10 <carloseduardoramirez10@gmail.com>
2025-05-27 16:39:53 -0400

    Agregar devtools a useAppStore para mejorar la depuración del estado

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,9 +1,10 @@
 import {create} from 'zustand'
+import {devtools} from 'zustand/middleware'
 import { createRecipeSlice, type RecipeSliceType } from './recipeSlice'
 // Este archivo define el store de Zustand para la aplicación
 //(...a) Es una sintaxis de JavaScript/TypeScript que captura todos los argumentos pasados a la función en un array llamado a
 //paso esta función create((...a) a createRecipeSlice(...a) para que use el store de recetas
 // ...a toma una copia de todos los argumentos , get, set y api etc. y los pasa a createRecipeSlice
-export const useAppStore = create<RecipeSliceType>((...a) => ({
+export const useAppStore = create<RecipeSliceType>()(devtools((...a) => ({
     ...createRecipeSlice(...a)
-}))
+})))
