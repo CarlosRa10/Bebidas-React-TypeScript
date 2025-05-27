cf0531d0742bb3d4a8fd392cee20c15a7e507c90
carlosra10 <carloseduardoramirez10@gmail.com>
2025-05-26 21:12:07 -0400

    Agregar comentario explicativo en useAppStore.ts para mejorar la claridad del código

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,6 +1,6 @@
 import {create} from 'zustand'
 import { createRecipeSlice } from './recipeSlice'
-
+// Este archivo define el store de Zustand para la aplicación
 export const useAppStore = create((...a) => ({
     ...createRecipeSlice(...a)
 }))
