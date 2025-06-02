575a2a584a73d09b907964d7df917d192434099e
carlosra10 <carloseduardoramirez10@gmail.com>
2025-06-01 23:21:09 -0400

    Agregar comentario descriptivo en IndexPage para mayor claridad

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,7 +1,7 @@
 
 export default function IndexPage() {
 
- 
+ // este archivo es el punto de entrada de la aplicación, aquí se renderiza el componente principal
   return (
     <>  
       <h1>Inicio</h1>
