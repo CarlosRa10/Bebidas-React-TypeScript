86405c6924c529a0755f6a7ecfcd094053168ac9
carlosra10 <carloseduardoramirez10@gmail.com>
2025-06-03 22:42:28 -0400

    Agregar comentario descriptivo en index.ts para mayor claridad

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 import {z} from 'zod';
 import { CategoriesAPIResponseSchema, SearchFilterSchema } from '../utils/recipes-schema';
 
+// este archivo es el archivo de tipos, aquí se definen los tipos que se van a utilizar en la aplicación
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>
 
 export type SearchFilter = z.infer<typeof SearchFilterSchema>
