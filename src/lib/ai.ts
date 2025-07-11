7254628a459124503be18ca6870c378744af8c52
CarlosRa10 <carloseduardoramirez10@gmail.com>
2025-07-10 22:06:15 -0400

    Eliminar espacio innecesario en la importación de createOpenRouter

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -1,4 +1,4 @@
-import { createOpenRouter } from '@openrouter/ai-sdk-provider'//importar la dependencia de openrouter
+import {createOpenRouter} from '@openrouter/ai-sdk-provider'//importar la dependencia de openrouter
 
 export const openrouter = createOpenRouter({
     apiKey: import.meta.env.VITE_OPENROUTER_KEY, //llamar a la variable de entorno
