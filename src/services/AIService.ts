05b00950c2a4e7fec13d226ba638deca0e803d7a
CarlosRa10 <carloseduardoramirez10@gmail.com>
2025-07-13 00:59:30 -0400

    Refactor AIService y aiSlice para mejorar la legibilidad y depuración, incluyendo comentarios y activando logs de consola.

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -1,4 +1,4 @@
-import { openrouter } from '@openrouter/ai-sdk-provider'
+import { openrouter } from '../lib/ai'// importar la función openrouter desde el archivo ai.ts
 import {streamText} from 'ai'//simplifica código para llamar a la API de AI - apiReference: https://ai.dev/docs/api-reference/stream-text
 //con el apikey sabra quienes somos 
 //con el prompt sabra que le estamos preguntando 
@@ -11,9 +11,12 @@ export default {
             // Aquí se configura el modelo de AI que se va a utilizar - objeto de configuración
             // modelo de AI que se va a utilizar
             model: openrouter('meta-llama/llama-3.3-70b-instruct:free'),
+            // model: openrouter('google/gemini-2.5-pro-exp-03-25:free'),
+            // model: openrouter('deepseek/deepseek-chat-v3-0324:free'),
+            // model: openrouter('google/gemma-3-4b-it:free'),
             prompt, // prompt es algo que espera streamText, es el texto que le vamos a pasar - prompt:prompt si se llaman igual solo prompt
         })  
-        //console.log(result);
+        console.log(result);
         return result.textStream
     }
 }
--- src/stores/aiSlice.ts
+++ src/stores/aiSlice.ts
@@ -9,7 +9,17 @@ export type AISlice = {
 export const createAISlice : StateCreator<AISlice,[],[],AISlice> = () => ({
     recipe: '',
     generateRecipe: async (prompt) => {
-        //console.log(prompt);
+        console.log(prompt);
         const data = await AIService.generateRecipe(prompt);
+        console.log('2. ¿Se obtuvo data?', Boolean(data)); // <-- Verifica si hay data
+        //ejecuta el for mientras el data siga teniendo datos
+        console.log('Tipo de data:', typeof data);
+        console.log('Es iterable?', Symbol.asyncIterator in data);
+        console.log('Es ReadableStream?', data instanceof ReadableStream);
+        console.log('Propiedades de data:', Object.keys(data));
+        console.log(data);
+        for await (const textPart of data) {
+           console.log(textPart);
+        }   
     }
 })
